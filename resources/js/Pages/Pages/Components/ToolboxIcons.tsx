0a069921d125ef4a0892dc09748fbce0accb7805
eugen <eugene@coreg.software>
2025-10-24 11:31:14 +0200

    New Editor

---
+++ resources/js/Pages/Pages/Components/ToolboxIcons.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+
+export const ButtonIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18" {...props}>
+        <path fill="#707070" d="M3 8.5a.5.5 0 0 0-.5.5v3a1.5 1.5 0 0 0 1.5 1.5h9a1.5 1.5 0 0 0 1.5-1.5v-3a.5.5 0 0 0-.5-.5H3zM2 7.5V6a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v1.5" />
+    </svg>
+);
+
+export const ContainerIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
+        <path fill="none" d="M0 0h24v24H0z"/>
+        <path d="M20 16h2v6h-6v-2H8v2H2v-6h2V8H2V2h6v2h8V2h6v6h-2v8zm-2 0V8h-2V6H8v2H6v8h2v2h8v-2h2zM4 4v2h2V4H4zm0 14v2h2v-2H4zM18 4v2h2V4h-2zm0 14v2h2v-2h-2z"/>
+    </svg>
+);
+
+export const LinkIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18" {...props}>
+    <path fill="#707070" d="M7.5,11h-3a2.5,2.5,0,0,1,0-5h3a.5.5,0,0,0,0-1h-3a3.5,3.5,0,0,0,0,7h3a.5.5,0,0,0,0-1Zm7-4h-3a.5.5,0,0,0,0,1h3a2.5,2.5,0,0,1,0,5h-3a.5.5,0,0,0,0,1h3a3.5,3.5,0,0,0,0-7Zm-9.9,4.6a.5.5,0,0,0,.7-.7L6.7,9.3a.5.5,0,0,0-.7.7Zm6.6-3.6a.5.5,0,0,0-.7.7l1.5,1.5a.5.5,0,0,0,.7-.7Z" />
+  </svg>
+);
+
+
+export const RectangleIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18" {...props}>
+    <path fill="#707070" d="M1,2.5v13a.5.5,0,0,0,.5.5h15a.5.5,0,0,0,.5-.5V2.5a.5.5,0,0,0-.5-.5H1.5A.5.5,0,0,0,1,2.5ZM16,15H2V3H16Z" />
+  </svg>
+);
+
+export const RedoIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18" {...props}>
+        <path fill="#707070" d="M9 4.5a4.5 4.5 0 1 1-4.473 5.5H3.21l2.147-2.146-.707-.708L1.5 10.5l3.15 3.146.707-.708L3.21 11.5h1.317A5.5 5.5 0 1 0 9 4.5z"/>
+    </svg>
+);
+
+
+export const TextIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18" {...props}>
+        <path fill="#707070" d="M3 2.5a.5.5 0 0 0 0 1h5.5v13a.5.5 0 0 0 1 0v-13H15a.5.5 0 0 0 0-1H3z"/>
+    </svg>
+);
+
+
+export const TextFillIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18" {...props}>
+        <path fill="#707070" d="M3 2.5a.5.5 0 0 0 0 1h5.5v13a.5.5 0 0 0 1 0v-13H15a.5.5 0 0 0 0-1H3zM4 4.5h10v1H4zm0 3h10v1H4zm0 3h10v1H4zm0 3h10v1H4z"/>
+    </svg>
+);
+
+
+
+
+export const UndoIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18" {...props}>
+        <path fill="#707070" d="M9 4.5a4.5 4.5 0 1 0 4.473 5.5h1.317l-2.147-2.146.707-.708L16.5 10.5l-3.15 3.146-.707-.708L14.79 11.5h-1.317A5.5 5.5 0 1 1 9 4.5z"/>
+    </svg>
+);
+
+
+export const VideoFillIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18" {...props}>
+        <path fill="#707070" d="M15.5 3h-13A1.5 1.5 0 0 0 1 4.5v9A1.5 1.5 0 0 0 2.5 15h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 15.5 3zm-9 10.5v-7l6 3.5-6 3.5z"/>
+    </svg>
+);
+
+
+export const VideoLineIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18" {...props}>
+        <path fill="#707070" d="M15.5 3h-13A1.5 1.5 0 0 0 1 4.5v9A1.5 1.5 0 0 0 2.5 15h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 15.5 3zm.5 10.5a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 .5.5v9zm-9-8v7l6-3.5-6-3.5z"/>
+    </svg>
+);
+
+export const YoutubeIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
+    <path fill="#707070" d="M10 9.5v5l4-2.5-4-2.5zM21 7.5s-.2-1.4-.8-2c-.8-.8-1.7-.8-2.1-.9C16.7 4.4 12 4.4 12 4.4s-4.7 0-5.9.2c-.4 0-1.3.1-2.1.9-.6.6-.8 2-.8 2S2 9.1 2 10.6v2.8C2 15.9 2.2 17.3 2.2 17.3s.2 1.4.8 2c.8.8 1.9.8 2.4.9 1.7.1 7.6.2 7.6.2s4.7 0 6-.2c.4 0 1.3.1 2.1-.9.6-.6.8-2 .8-2s.2-1.4.2-2.9v-2.8c0-1.5-.2-2.9-.2-2.9z" />
+  </svg>
+);
+
+
